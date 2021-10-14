aa2b5bdc7ded3e32e028858eadab3172cb93a157
DanyalNazid <92511251+DanyalNazid@users.noreply.github.com>
2021-10-14 16:28:18 +0800

    first update
    update of initial files

---
+++ nav.php
@@ -0,0 +1,40 @@
+<h1>Police Emergency Service System</h1>
+
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+  
+}
+
+li a {
+  display:block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: salmon;
+}
+		
+	
+</style>
+
+<nav> 
+	<ul>
+   <li> <a href="logcall.php">Log Call</a> </li>
+   <li> <a href="Update.php">Update</a> </li>
+   <li> <a href="#">Report</a> </li>
+   <li> <a href="#">History</a> </li>
+	</ul>
+  </nav> 
